add04e5a80affef2c27dc3174681c21116372a74
richiexuetang <richard772794659@gmail.com>
2022-11-08 11:06:55 -0800

    set up user model

--- backend/lull/settings.py
+++ backend/lull/settings.py
@@ -37,7 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # my apps
+    'users',
 ]
+
+AUTH_USER_MODEL = 'users.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
